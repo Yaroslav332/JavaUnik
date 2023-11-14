505f2ea9c63cce9424552e602c354b2893894cbe
MegaMaks <Ryzhkov0@yandex.ru>
2023-11-14 12:23:17 +0500

    logic +

--- Bot/src/main/java/ru/UnspokenRizz/TelegramBot/RoomController.java
+++ Bot/src/main/java/ru/UnspokenRizz/TelegramBot/RoomController.java
@@ -1,4 +1,12 @@
 package ru.UnspokenRizz.TelegramBot;
 
+import ru.UnspokenRizz.TelegramBot.logic.Room;
+
+import java.util.HashMap;
+
 public class RoomController {
+    HashMap<Long, Room> RoomContainer = new HashMap<>();
+    public Room CreateRoom(Long user){
+        return null; };
+    public void DeleteRoom(Long user){}
 }
